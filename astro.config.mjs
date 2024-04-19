5c1da3fadb99c8facce6fb60f5453e4caa2ebc85
David J. Bradshaw <dave@bradshaw.net>
2024-04-19 11:46:34 +0100

    Add React support and Install component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,76 +1,84 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "http://iframe-resizer.com",
-  integrations: [
-    starlight({
-      title: "iframe-resizer",
-      components: {
-        SocialIcons: "./src/components/socials.astro",
+  integrations: [starlight({
+    title: "iframe-resizer",
+    components: {
+      SocialIcons: "./src/components/socials.astro"
+    },
+    social: {
+      github: "https://github.com/davidjbradshaw/iframe-resizer"
+    },
+    sidebar: [{
+      label: "Introduction",
+      link: "introduction"
+    }, {
+      label: "Licenses",
+      link: "license"
+      // items: [
+      //   { label: "Comercial License", link: "/license#comercial" },
+      //   { label: "Open Source License", link: "/license#open" },
+      // ],
+    }, {
+      label: "Getting Started",
+      link: "getting_started"
+      // items: [
+      //   { label: "Same Domain", link: "/license#comercial" },
+      //   { label: "Cross Domain", link: "/license#open" },
+      // ],
+    }, {
+      label: "API",
+      items: [{
+        label: "Parent Page",
+        link: "/api/parent"
+      }, {
+        label: "Child Page",
+        link: "/api/child"
+      }]
+    }, {
+      label: "Frameworks and Libraries",
+      items: [{
+        label: "Overview",
+        link: "/frameworks/overview"
+      }, {
+        label: "React",
+        link: "/frameworks/react"
+      }, {
+        label: "Vue",
+        link: "/frameworks/vue3"
       },
-      social: {
-        github: "https://github.com/davidjbradshaw/iframe-resizer",
+      // { label: "Vue 2", link: "/frameworks/vue2" },
+      {
+        label: "jQuery",
+        link: "/frameworks/jquery"
+      }, {
+        label: "Google Apps Scripts",
+        link: "/frameworks/google_apps_script"
       },
-      sidebar: [
-        {
-          label: "Introduction",
-          link: "introduction",
-        },
-        {
-          label: "Licenses",
-          link: "license",
-          // items: [
-          //   { label: "Comercial License", link: "/license#comercial" },
-          //   { label: "Open Source License", link: "/license#open" },
-          // ],
-        },
-        {
-          label: "Getting Started",
-          link: "getting_started",
-          // items: [
-          //   { label: "Same Domain", link: "/license#comercial" },
-          //   { label: "Cross Domain", link: "/license#open" },
-          // ],
-        },
-        {
-          label: "API",
-          items: [
-            { label: "Parent Page", link: "/api/parent" },
-            { label: "Child Page", link: "/api/child" },
-          ],
-        },
-        {
-          label: "Frameworks and Libraries",
-          items: [
-            { label: "Overview", link: "/frameworks/overview" },
-            { label: "React", link: "/frameworks/react" },
-            { label: "Vue", link: "/frameworks/vue3" },
-            // { label: "Vue 2", link: "/frameworks/vue2" },
-            { label: "jQuery", link: "/frameworks/jquery" },
-            {
-              label: "Google Apps Scripts",
-              link: "/frameworks/google_apps_script",
-            },
-            // { label: "Angular", link: "/frameworks/angular" },
-            { label: "Ember", link: "/frameworks/ember" },
-            { label: "Framework API", link: "/frameworks/api" },
-          ],
-        },
-        {
-          label: "Troubleshooting",
-          link: "troubleshooting",
-        },
-        {
-          label: "Upgrade to V5",
-          link: "upgrade",
-        },
-        // {
-        //   label: "Reference",
-        //   autogenerate: { directory: "reference" },
-        // },
-      ],
-    }),
-  ],
+      // { label: "Angular", link: "/frameworks/angular" },
+      {
+        label: "Ember",
+        link: "/frameworks/ember"
+      }, {
+        label: "Framework API",
+        link: "/frameworks/api"
+      }]
+    }, {
+      label: "Troubleshooting",
+      link: "troubleshooting"
+    }, {
+      label: "Upgrade to V5",
+      link: "upgrade"
+    }
+    // {
+    //   label: "Reference",
+    //   autogenerate: { directory: "reference" },
+    // },
+    ]
+  }), react()]
 });
