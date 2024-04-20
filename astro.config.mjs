ea0516b1eb474566dcf77e0fab48acb6175d448e
David J. Bradshaw <dave@bradshaw.net>
2024-04-20 20:29:38 +0100

    Add favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,80 +5,92 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://iframe-resizer.com",
-  integrations: [starlight({
-    title: "iframe-resizer",
-    components: {
-      SocialIcons: "./src/components/socials.astro"
-    },
-    social: {
-      github: "https://github.com/davidjbradshaw/iframe-resizer"
-    },
-    sidebar: [{
-      label: "Introduction",
-      link: "introduction"
-    }, {
-      label: "Licenses",
-      link: "license"
-      // items: [
-      //   { label: "Comercial License", link: "/license#comercial" },
-      //   { label: "Open Source License", link: "/license#open" },
-      // ],
-    }, {
-      label: "Getting Started",
-      link: "getting_started"
-      // items: [
-      //   { label: "Same Domain", link: "/license#comercial" },
-      //   { label: "Cross Domain", link: "/license#open" },
-      // ],
-    }, {
-      label: "API",
-      items: [{
-        label: "Parent Page",
-        link: "/api/parent"
-      }, {
-        label: "Child Page",
-        link: "/api/child"
-      }]
-    }, {
-      label: "Frameworks and Libraries",
-      items: [{
-        label: "Overview",
-        link: "/frameworks/overview"
-      }, {
-        label: "React",
-        link: "/frameworks/react"
-      }, {
-        label: "Vue",
-        link: "/frameworks/vue3"
+  site: "https://iframe-resizer.com",
+  integrations: [
+    starlight({
+      title: "iframe-resizer",
+      favicon: "/favicon.ico",
+      components: {
+        SocialIcons: "./src/components/socials.astro",
       },
-      // { label: "Vue 2", link: "/frameworks/vue2" },
-      {
-        label: "jQuery",
-        link: "/frameworks/jquery"
-      }, {
-        label: "Google Apps Scripts",
-        link: "/frameworks/google_apps_script"
+      social: {
+        github: "https://github.com/davidjbradshaw/iframe-resizer",
       },
-      // { label: "Angular", link: "/frameworks/angular" },
-      {
-        label: "Ember",
-        link: "/frameworks/ember"
-      }, {
-        label: "Framework API",
-        link: "/frameworks/api"
-      }]
-    }, {
-      label: "Troubleshooting",
-      link: "troubleshooting"
-    }, {
-      label: "Upgrade to V5",
-      link: "upgrade"
-    }
-    // {
-    //   label: "Reference",
-    //   autogenerate: { directory: "reference" },
-    // },
-    ]
-  }), react()]
+      sidebar: [
+        {
+          label: "Introduction",
+          link: "introduction",
+        },
+        {
+          label: "Licenses",
+          link: "license",
+        },
+        {
+          label: "Getting Started",
+          link: "getting_started",
+        },
+        {
+          label: "API",
+          items: [
+            {
+              label: "Parent Page",
+              link: "/api/parent",
+            },
+            {
+              label: "Child Page",
+              link: "/api/child",
+            },
+          ],
+        },
+        {
+          label: "Frameworks and Libraries",
+          items: [
+            {
+              label: "Overview",
+              link: "/frameworks/overview",
+            },
+            {
+              label: "React",
+              link: "/frameworks/react",
+            },
+            {
+              label: "Vue",
+              link: "/frameworks/vue3",
+            },
+            // { label: "Vue 2", link: "/frameworks/vue2" },
+            {
+              label: "jQuery",
+              link: "/frameworks/jquery",
+            },
+            {
+              label: "Google Apps Scripts",
+              link: "/frameworks/google_apps_script",
+            },
+            // { label: "Angular", link: "/frameworks/angular" },
+            {
+              label: "Ember",
+              link: "/frameworks/ember",
+            },
+            {
+              label: "Framework API",
+              link: "/frameworks/api",
+            },
+          ],
+        },
+        {
+          label: "Troubleshooting",
+          link: "troubleshooting",
+        },
+        {
+          label: "Upgrade to V5",
+          link: "upgrade",
+        },
+        // {
+        //   label: "Reference",
+        //   autogenerate: { directory: "reference" },
+        // },
+      ],
+    }),
+    react(),
+  ],
 });
