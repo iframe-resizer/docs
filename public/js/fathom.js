651db399dc2c5fdc0b359ce5f79da4e6804db122
David J. Bradshaw <dave@bradshaw.net>
2024-05-21 17:14:24 +0100

    Fathom contact

--- public/js/fathom.js
+++ public/js/fathom.js
@@ -1,13 +1,19 @@
 document.querySelectorAll('a').forEach((item) => {
   if (item.href.includes("github.com")) {
     item.addEventListener("click", (event) => {
-      fathom.trackEvent("GitHub");
+      fathom?.trackEvent("GitHub");
     });
   }
 
   if (item.href.includes("viewerjs.org")) {
     item.addEventListener("click", (event) => {
-      fathom.trackEvent("PDF/ODF");
+      fathom?.trackEvent("PDF/ODF");
+    });
+  }
+
+  if (item.href.includes("mail")) {
+    item.addEventListener("click", (event) => {
+      fathom?.trackEvent("Email Contact");
     });
   }
 })
