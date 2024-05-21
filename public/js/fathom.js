812e569205238a35113e40eee6c4f8845249536a
David J. Bradshaw <dave@bradshaw.net>
2024-05-21 16:45:11 +0100

    Tag viewerjs

--- public/js/fathom.js
+++ public/js/fathom.js
@@ -1,8 +1,14 @@
 document.querySelectorAll('a').forEach((item) => {
-  if (item.href.includes('github.com')) {
-    item.addEventListener('click', (event) => {
-      fathom.trackEvent('GitHub');
-    })
+  if (item.href.includes("github.com")) {
+    item.addEventListener("click", (event) => {
+      fathom.trackEvent("GitHub");
+    });
+  }
+
+  if (item.href.includes("viewerjs.org")) {
+    item.addEventListener("click", (event) => {
+      fathom.trackEvent("PDF/ODF");
+    });
   }
 })
 
