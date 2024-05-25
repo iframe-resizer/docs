91e089a0a81962d8e94f826a2cdd83f7ed86bb15
David J. Bradshaw <dave@bradshaw.net>
2024-05-25 18:02:26 +0100

    Add track mdn page out

--- public/js/fathom.js
+++ public/js/fathom.js
@@ -1,3 +1,5 @@
+const last = (arr) => arr[arr.length - 1];
+
 const trackLinkOut = (item) => (target) =>
   item.addEventListener('click', () => {
     fathom?.trackEvent(`$(target) from ${document.title}`);
@@ -19,13 +21,13 @@ document.querySelectorAll('a').forEach((item) => {
       return trackLink('GitHub')
 
     case item.href.includes('mozilla.org'):
-      return trackLink('Mozilla')
+      return trackLink(`Mozilla ${last(item.href.split('/'))}`)
 
     case item.href.includes('viewerjs.org'):
       return trackLink('PDF/ODF Viewer')
 
     case item.href.includes('mail'):
-      return trackLink('Contact email')
+      return trackLink('Contact')
 
     case item.href.includes('http'):
       return trackLink(`External Link: ${item.href}`)
