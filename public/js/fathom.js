34207a53a4760ac5712b0ad75be678532bea2b04
David J. Bradshaw <dave@bradshaw.net>
2024-05-25 19:05:47 +0100

    Only track the clicked link

--- public/js/fathom.js
+++ public/js/fathom.js
@@ -1,21 +1,23 @@
 const last = (arr) => arr[arr.length - 1];
 
-const trackLink =  (target) =>
-  addEventListener('click', () => {
-    fathom?.trackEvent(`${target} from ${document.title.split(' | ')[0].replace('IFRAME-RESIZER', 'Homepage')}`);
-  });
+const trackLinkExt =  (el) => (target) =>
+  el.addEventListener('click', () => 
+    console.log(`Tracking: ${target} from ${document.title.split(' | ')[0].replace('IFRAME-RESIZER', 'Homepage')}`) ||
+    window.fathom?.trackEvent(`${target} from ${document.title.split(' | ')[0].replace('IFRAME-RESIZER', 'Homepage')}`)
+  )
 
 if (document.title === '404') {
-  fathom?.trackEvent(`404: ${document.referrer || 'Direct'}`)
+  window.fathom?.trackEvent(`404: ${document.referrer || 'Direct'}`)
 }
 
 document.querySelectorAll('a').forEach((a) => {
   const { href } = a
+  const trackLink = trackLinkExt(a)
 
   if (
-    false === href.includes("iframe-resizer.com") &&
-    false === href.includes("localhost") &&
-    false === href.includes("lemon")
+    !href.includes("iframe-resizer.com") &&
+    !href.includes("localhost") &&
+    !href.includes("lemon")
   ) {
     a.target = "_blank"
   }
