ae64111c8d8ad8deb1e33bdba66e09e7c89167b7
David J. Bradshaw <dave@bradshaw.net>
2024-05-25 17:56:59 +0100

    Tidy out link tracker

--- public/js/fathom.js
+++ public/js/fathom.js
@@ -1,34 +1,34 @@
-const trackLinkOut = (item) => (includes, log) =>
-  item.addEventListener(includes, () => {
-    fathom?.trackEvent(log);
-  })
+const trackLinkOut = (item) => (target) =>
+  item.addEventListener('click', () => {
+    fathom?.trackEvent(`$(target) from ${document.title}`);
+  });
 
-if (document.title === "404") {
-  fathom?.trackEvent(`404: ${document.referrer || "Direct"}`)
+if (document.title === '404') {
+  fathom?.trackEvent(`404: ${document.referrer || 'Direct'}`)
 }
 
-document.querySelectorAll("a").forEach((item) => {
+document.querySelectorAll('a').forEach((item) => {
   const trackLink = trackLinkOut(item)
   
-  if (item.href.includes("http")) {
-    item.target = "_blank"
+  if (item.href.includes('http')) {
+    item.target = '_blank'
   }
 
   switch (true) {
-    case item.href.includes("github.com"):
-      return trackLink("click", `GitHub from ${document.title}`)
+    case item.href.includes('github.com'):
+      return trackLink('GitHub')
 
-    case item.href.includes("mozilla.org"):
-      return trackLink("click", `Mozilla from ${document.title}`)
+    case item.href.includes('mozilla.org'):
+      return trackLink('Mozilla')
 
-    case item.href.includes("viewerjs.org"):
-      return trackLink("click", `PDF/ODF Viewer from ${document.title}`)
+    case item.href.includes('viewerjs.org'):
+      return trackLink('PDF/ODF Viewer')
 
-    case item.href.includes("mail"):
-      return trackLink("click", `Contact email from ${document.title}`)
+    case item.href.includes('mail'):
+      return trackLink('Contact email')
 
-    case item.href.includes("http"):
-      return trackLink("click", `External Link: ${item.href} from ${document.title}`)
+    case item.href.includes('http'):
+      return trackLink(`External Link: ${item.href}`)
     
     default:
       return
